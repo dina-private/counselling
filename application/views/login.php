436dae544ed418478bcac9ca0ac12e070b39153a
Dinanath Thakur <kumardina023@gmail.com>
2019-07-19 23:52:56 +0530

    login page

--- application/views/login.php
+++ application/views/login.php
@@ -1,13 +1,45 @@
 <html>
 <head>
-    <title>Login</title>
+    <title>Login Page</title>
 </head>
 <body>
-<form action="">
-    <label>UserName:</label>
-    <input name="username">
-    <label>Password:</label>
-    <input name="password">
+<form name="loginForm" method="post" action="login.php">
+    <table width="20%" bgcolor="0099CC" align="center">
+
+        <tr>
+            <td colspan=2>
+                <h3 size=4><b>Login</b></h3>
+            </td>
+        </tr>
+
+        <tr>
+            <td>Username:</td>
+            <td><input type="text" size=25 name="userid"></td>
+        </tr>
+
+        <tr>
+            <td>Password:</td>
+            <td><input type="Password" size=25 name="pwd"></td>
+        </tr>
+
+        <tr>
+            <td><input type="Reset"></td>
+            <td><input type="submit" onclick="return check(this.form)" value="Login"></td>
+        </tr>
+
+    </table>
 </form>
+<script language="javascript">
+    function check(form) {
+
+        if (form.userid.value == "Roseindia" && form.pwd.value == "Roseindia") {
+            return true;
+        } else {
+            alert("Error Password or Username")
+            return false;
+        }
+    }
+</script>
+
 </body>
 </html>
